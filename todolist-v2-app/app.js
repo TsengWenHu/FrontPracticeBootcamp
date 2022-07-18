d8af8e1658de24be52286d0d3df760961bdc68e3
TsengWenHu <rubyrubyhuhu@gmail.com>
2022-07-18 17:44:23 +0800

    20220718

--- todolist-v2-app/app.js
+++ todolist-v2-app/app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
 const _ = require("lodash");
+const myModule = require("./myOwnModule")
 
 const app = express();
 
@@ -13,7 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 // connect to mongoDB Atlas
-mongoose.connect("mongodb+srv://admin-ruby:Test123@cluster0.lrl5i.mongodb.net/todolistDB", {useNewUrlParser: true});
+mongoose.connect(myModule.accessURL(), {useNewUrlParser: true});
 
 const itemsSchema = {
   name: String
